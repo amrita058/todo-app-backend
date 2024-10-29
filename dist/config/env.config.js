6f24a84fc0265e42fd6050aa088522795fef343b
amrita <amritachi58@gmail.com>
2024-10-29 09:52:32 +0545

    dist removed

--- dist/config/env.config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.env = void 0;
-console.log("process", process.env.PORT);
-exports.env = {
-    PORT: process.env.PORT,
-    URI: process.env.URI,
-    SECRET_KEY: process.env.SECRET_KEY,
-};
